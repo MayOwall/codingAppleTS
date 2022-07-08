8bb4ccfe5c28da2d981b4888503bfd443580fe27
Owall <97934878+MayOwall@users.noreply.github.com>
2022-07-08 15:55:04 +0900

    Lec1

---
+++ Part1/index.js
@@ -0,0 +1,24 @@
+// 타입의 종류 : string, number, boolean, null, undefined, [], {}...
+// 문자열 타입 지정
+let 이름 = "kim";
+// 배열 타입 지정
+let 이름배열 = ["kim", "park"];
+// 객체 타입 지정
+let 이름객체 = { name: "kim" };
+// 여러개의 타입 지정
+let 이름여러개 = "kim";
+이름여러개 = 123;
+let 이름변수 = "이름";
+// 함수에 타입 정해주기
+function 함수(x) {
+    return x;
+}
+함수("문자열");
+let owall = [123, true];
+let may = { name: "may", location: "seoul" };
+// class 타입 지정
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+}

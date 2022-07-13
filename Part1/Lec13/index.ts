e288d89c89aff3de8fa7a692b2eccf0ddb5bda8c
Owall <97934878+MayOwall@users.noreply.github.com>
2022-07-13 16:12:14 +0900

    Lec13

--- Part1/Lec13/index.ts
+++ Part1/Lec13/index.ts
@@ -16,4 +16,29 @@
 
   let person1 = new Person("may");
   let person2 = new Person("owall");
+
+  // 숙제 1
+  class Car {
+    model: string;
+    price: number;
+    constructor(model: string, price: number) {
+      this.model = model;
+      this.price = price;
+    }
+    tax(): number {
+      return this.price * 0.1;
+    }
+  }
+
+  // 숙제 2
+  class Word {
+    num: number[];
+    str: string[];
+    arguments: Element;
+
+    constructor(...params: any[]) {
+      this.num = params.filter((x: number | string) => typeof x === "number");
+      this.str = params.filter((x: number | string) => typeof x === "string");
+    }
+  }
 }
